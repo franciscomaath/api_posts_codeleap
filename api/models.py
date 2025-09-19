8a85584f6a8172d5588d4c1dfbb77b4d3a28da59
glubao <40120350+glubao@users.noreply.github.com>
2025-09-19 09:05:09 -0300

    feat: Create Post model and database migration

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_datetime']
+    
+    def __str__(self):
+        return f'"{self.title}" by @{self.username}'
